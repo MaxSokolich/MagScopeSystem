a21936d00ba1184f10e33194cbc729f0af78f654
max <sokolich@udel.edu>
2023-03-08 17:14:44 -0500

    testing thread writer

--- tests/sepreateprocessvideowrite.py
+++ tests/sepreateprocessvideowrite.py
@@ -2,9 +2,17 @@ import cv2
 import multiprocessing as mp
 import EasyPySpin
 
+# Initialize the video capture object
+cap = EasyPySpin.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FPS, 5)
+
+
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 # Define the codec and create a VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (640, 480))
+out = cv2.VideoWriter('output.mp4', fourcc, 5, (w, h))
 
 # Define the function to write frames to the output file
 def write_frames(queue):
@@ -19,10 +27,10 @@ queue = mp.Queue()
 process = mp.Process(target=write_frames, args=(queue,))
 process.start()
 
-# Initialize the video capture object
-cap = EasyPySpin.VideoCapture(0)
+
 
 # Loop through the frames and put them in the queue
+n=0
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
@@ -41,8 +49,10 @@ while cap.isOpened():
 # Put None in the queue to signal the end of frames
 queue.put(None)
 
+
 # Release the resources
-cap.release()
 process.join()
+cap.release()
 out.release()
+print("release writer")
 cv2.destroyAllWindows()
