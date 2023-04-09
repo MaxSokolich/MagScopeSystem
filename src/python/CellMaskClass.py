979bf156566930a3ce1a6819b90bacee7cd5157f
Max Sokolich <50302377+MaxSokolich@users.noreply.github.com>
2023-04-09 15:17:01 -0400

    Update CellMaskClass.py

--- src/python/CellMaskClass.py
+++ src/python/CellMaskClass.py
@@ -20,6 +20,7 @@ class MAXS_SAM:
 
         self.predictor = SamPredictor(sam)
 
+        self.mask_generator = SamAutomaticMaskGenerator(sam)
         self.mask_generator_2 = SamAutomaticMaskGenerator(
             model=sam,
             points_per_side=32,
@@ -32,7 +33,6 @@ class MAXS_SAM:
 
         print("SAM loaded successfully!")
 
-        self.mask_list = []
         self.reference_point = [0,0]
         self.final_mask = None
         self.cell_count = 0
@@ -43,39 +43,61 @@ class MAXS_SAM:
         if event == cv2.EVENT_LBUTTONDOWN:
             self.cell_count +=1
             self.reference_point = [x,y]
-            if self.cell_count >0:
-                self.mask_list.append(params["msks"])
+            self.final_mask += params["msks"]
+
+            
+            
+        
      
+
+
+    def auto_mask(self,frame):
+        #find all masks
+        start=time.time()
+        print("finding all masks...")
+        all_masks = self.mask_generator_2.generate(frame)
+        print("done - {} masks in {}".format(len(all_masks), round(time.time()-start,2)))
+
+
+        #create final mask
+        final_mask = all_masks[0]["segmentation"].astype(np.uint8)
+        for i in range(len(all_masks)):
+            final_mask += all_masks[i]["segmentation"].astype(np.uint8)
+
+        final_mask *= 255
+        self.final_mask = final_mask
+
+
+        #create viewable mask to edit in next section
+        temp_mask = cv2.threshold(final_mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        viewable_mask = frame.copy()
+        viewable_mask[temp_mask == 255] = (36,255,12)
+
+        #return the viewable_mask
+        return viewable_mask
+    
+
 
     def main(self, file):
 
         frame = cv2.imread(file)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
         print("Setting image predictor ...")
         self.predictor.set_image(frame)
         print("Image predictor set!")   
-         
-        cv2.namedWindow("img")  # name of CV2 window
-        cv2.setMouseCallback("img", self.mouse_points)
-
-        print("finding all masks...")
-        #all_masks = self.mask_generator_2.generate(frame)
-        print("done")
-
-        #for i in range(1,len(all_masks)):
-        #    bwmask = all_masks[i]["segmentation"]
-        #    self.mask_list.append(bwmask)
         
-        
+       
+        newframe = self.auto_mask(frame) 
 
+        cv2.namedWindow("update")  # name of CV2 window
+        cv2.setMouseCallback("update", self.mouse_points)
+        
         while True:
-            #for i in range(len(all_masks)):
-            #    bwmask = all_masks[i]["segmentation"].astype(np.uint8)
-            #    bwmask*=255
-            #    cnts, _ = cv2.findContours(bwmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            #    cv2.drawContours(frame, cnts, -1,(0,255,0), 3)
-
             if self.cell_count >0:
+                
+
+                #
                 input_point = np.array([self.reference_point])
                 input_label = np.array([1])
 
@@ -85,45 +107,36 @@ class MAXS_SAM:
                                             point_labels=input_label,
                                             multimask_output=True,
                                             )
-                #convert to cv2 format
+                #convert to opecv format
                 best_mask = masks[np.argmax(scores)]
-                params = {"msks": best_mask}
-
-                #create black and white mask
                 bwmask = best_mask.astype(np.uint8)
-                cv2.setMouseCallback("img", self.mouse_points, params)
-
                 bwmask*=255
+                params = {"msks": bwmask}
+                cv2.setMouseCallback("update", self.mouse_points, params)
 
+                
+                #show contours
 
                 cnts, _ = cv2.findContours(bwmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                cv2.drawContours(frame, cnts, -1,(0,255,0), 3)
-            
-            cv2.imshow("img",frame)
+                cv2.drawContours(newframe, cnts, -1,(0,255,0), 3)
 
-            if cv2.waitKey(500) & 0xFF == ord('q'):
+            cv2.imshow("update",newframe)
+
+            if cv2.waitKey(100) & 0xFF == ord('q'):
                 break
 
         cv2.destroyAllWindows()
 
 
-        self.final_mask = self.mask_list[0].astype(np.uint8)
-
-        for i in range(len(self.mask_list)):
-            self.final_mask += self.mask_list[i].astype(np.uint8)
-
-        self.final_mask *= 255
-        cv2.imshow("img2",self.final_mask)
+        cv2.imshow("final",self.final_mask)
 
         if cv2.waitKey(0) & 0xFF == ord('q'):
             cv2.imwrite("cellmask.png", self.final_mask)
             cv2.destroyAllWindows()
         
 
-        
-    
 
 if __name__ == "__main__":
-    file = "/Users/bizzarohd/Desktop/Screen Shot 2023-04-08 at 6.57.24 PM.png"
+    file = "/Users/bizzarohd/Desktop/Screen Shot 2023-04-08 at 2.28.23 PM.png"
     detector = MAXS_SAM()
     detector.main(file)
