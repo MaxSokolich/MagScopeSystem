c8a83e349f21fb1be0d4de725be8e9c1e06b046a
Max Sokolich <50302377+MaxSokolich@users.noreply.github.com>
2023-04-21 15:48:29 -0400

    Update PIDforIndiviudal.py

--- src/tests/PIDforIndiviudal.py
+++ src/tests/PIDforIndiviudal.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 
 # Define the global control actuation field
 control_field = np.zeros((10,10,2))  # 10x10 grid, 2D vector field
@@ -19,32 +20,77 @@ markers[1,:] = [6,6]  # desired position for robot 2
 markers[2,:] = [9,9]  # desired position for robot 3
 
 # Define the local control law for each microrobot
-kp = 0.5  # proportional gain
-kd = 0.1  # derivative gain
-
-def local_control(robot, control_field, marker, kp, kd):
+kp = .5  # proportional gain
+kd = 1  # derivative gain
+ki = .001
+prev_error = 0
+integral_error = 0
+def local_control(robot, control_field, marker, kp, kd, ki, dt):
+    global prev_error, integral_error
     error = marker - robot
-    velocity = kp * error + kd * (error - robot.prev_error)
-    robot.prev_error = error
+
+    integral_error += error 
+
+    derivative_error = (error - prev_error) 
+
+    velocity = kp * error + ki * integral_error + kd * derivative_error
+
+
+    prev_error = error
     return velocity + control_field[int(robot[0]),int(robot[1]),:]
 
-# Move the microrobots to their desired positions
-num_steps = 100
-dt = 0.1
+# Initialize the Pygame environment
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Microrobot Control")
 
-for t in range(num_steps):
+# Define some colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+
+# Define a function to draw the microrobots and their markers on the screen
+def draw_robots(robots, markers):
+    for i in range(num_robots):
+        pygame.draw.circle(screen, blue, robots[i,:]*60+30, 10)
+        pygame.draw.circle(screen, red, markers[i,:]*60+30, 10)
+
+# Define a function to update the microrobot positions and velocities
+def update_robots(robots, markers, control_field, kp, kd, dt):
     for i in range(num_robots):
         # Update the microrobot position using the local control law
-        robots[i,:] += local_control(robots[i,:], control_field, markers[i,:], kp, kd) * dt
+        robots[i,:] += local_control(robots[i,:], control_field, markers[i,:], kp, kd, ki, dt) * dt
         
         # Wrap the microrobot position if it goes beyond the workspace boundaries
         robots[i,:] = np.mod(robots[i,:], np.array([10,10]))
-        
-    # Plot the microrobots and their markers (for visualization purposes)
-    plt.figure()
-    plt.quiver(control_field[:,:,0], control_field[:,:,1])
-    plt.plot(robots[:,0], robots[:,1], 'bo')
-    plt.plot(markers[:,0], markers[:,1], 'ro')
-    plt.xlim([0,10])
-    plt.ylim([0,10])
-    plt.show()
+
+# Define the main loop of the simulation
+running = True
+clock = pygame.time.Clock()
+num_steps = 1000
+dt = 0.01
+
+while running:
+    # Handle events (quit, keypresses, etc.)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    # Update the microrobot positions and velocities
+    update_robots(robots, markers, control_field, kp, kd, dt)
+    
+    # Clear the screen and draw the microrobots and their markers
+    screen.fill(white)
+    draw_robots(robots, markers)
+
+    # Update the display and wait for the next time step
+    pygame.display.flip()
+    clock.tick(30)
+
+    # Exit the loop if the desired number of time steps has been reached
+    num_steps -= 1
+    if num_steps == 0:
+        running = False
+
+pygame.quit()
